5e2650412c5d920d50dadabba861ddfe55482913
diz1 <dima-smirnov1@mail.ru>
2020-04-21 12:24:11 +0300

    lesson_19_done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 'use strict';
 window.addEventListener('DOMContentLoaded', () => {
+
+	// Таймер
 	const countTimer = (deadline, hoursElement, minutesElement, secondsElement) => {
 
 		const getElements = () => {
@@ -54,9 +56,87 @@ window.addEventListener('DOMContentLoaded', () => {
 	};
 
 	countTimer(
-		'21 april 2020',
+		'22 april 2020',
 		'#timer-hours',
 		'#timer-minutes',
 		'#timer-seconds'
 	);
+
+	// Меню
+	const toggleMenu = () => {
+		const btnMenu = document.querySelector('.menu'),
+			menu = document.querySelector('menu'),
+			closeBtn = document.querySelector('.close-btn'),
+			menuItem = menu.querySelectorAll('ul > li');
+
+		const menuHandler = () => {
+			menu.classList.toggle('active-menu');
+		};
+
+		btnMenu.addEventListener('click', menuHandler);
+		closeBtn.addEventListener('click', menuHandler);
+		menuItem.forEach(item => item.addEventListener('click', menuHandler));
+	};
+
+	toggleMenu();
+
+	// Поп-ап
+
+	const togglePopup = () => {
+		const popup = document.querySelector('.popup'),
+			popupBtn = document.querySelectorAll('.popup-btn'),
+			popupClose = document.querySelector('.popup-close'),
+			popupContent = popup.querySelector('.popup-content'),
+			popupStyles = getComputedStyle(popup);
+
+		// 	() => {
+		// 	popup.style.display = `block`;
+		// }));
+
+		const popupHandler = () => {
+			let count = -50;
+			if (popupStyles.display === `none`) {
+				popup.style.display = `block`;
+				popupContent.style.top = `${count}%`;
+
+				const timer = setInterval(() => {
+					count += 1;
+
+					popupContent.style.top = count + '%';
+
+					if (count > 20) clearInterval(timer);
+				}, 15);
+			} else if (popupStyles.display === `block`) {
+				count = 20;
+				const timer = setInterval(() => {
+					count -= 1;
+
+					popupContent.style.top = count + '%';
+
+					if (count === -50) clearInterval(timer);
+				}, 15);
+				setTimeout(() => popup.style.display = `none`, 1000);
+			}
+		};
+
+		popupBtn.forEach(item => item.addEventListener('click', popupHandler));
+		popupClose.addEventListener('click', popupHandler);
+
+		if (document.documentElement.clientWidth < 768) {
+			popupBtn.forEach(item => item.removeEventListener('click', popupHandler));
+			popupClose.removeEventListener('click', popupHandler);
+
+			popupBtn.forEach(item => item.addEventListener('click', () => {
+				popup.style.display = `block`;
+			}));
+			popupClose.addEventListener('click', () => {
+				popup.style.display = `none`;
+			});
+		} else {
+			popupBtn.forEach(item => item.addEventListener('click', popupHandler));
+			popupClose.addEventListener('click', popupHandler);
+		}
+	};
+
+	togglePopup();
 });
