b78be05d85042a2f56bdcf24c04e3b9be64efc1d
diz1 <dima-smirnov1@mail.ru>
2020-04-29 22:46:29 +0300

    lesson_25_done

--- js/script.js
+++ js/script.js
@@ -166,7 +166,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
 				draw(progress);
 
-				if (timeFraction < 1) {
+				if (timeFraction < 1 && document.documentElement.scrollTop < 830) {
 					requestAnimationFrame(animate);
 				}
 			});
@@ -189,6 +189,108 @@ window.addEventListener('DOMContentLoaded', () => {
 	};
 
 	scroll();
+
+
+	// Обработка форм
+	const sendForm = () => {
+		const message = {
+				error: `Что-то пошло не так`,
+				success: `Спасибо! Мы скоро с вами свяжемся!`,
+				status: document.createElement('div'),
+			},
+			forms = [...document.forms];
+		message.status.style.cssText = 'font-size: 2rem';
+
+		function maskPhone(selector, masked = '+7 (___) ___-__-__') {
+			const elem = document.querySelector(selector);
+
+			function mask(event) {
+				const keyCode = event.keyCode;
+				const template = masked,
+					def = template.replace(/\D/g, ""),
+					val = this.value.replace(/\D/g, "");
+				let i = 0,
+					newValue = template.replace(/[_\d]/g, function (a) {
+						return i < val.length ? val.charAt(i++) || def.charAt(i) : a;
+					});
+				i = newValue.indexOf("_");
+				if (i != -1) {
+					newValue = newValue.slice(0, i);
+				}
+				let reg = template.substr(0, this.value.length).replace(/_+/g,
+					function (a) {
+						return "\\d{1," + a.length + "}";
+					}).replace(/[+()]/g, "\\$&");
+				reg = new RegExp("^" + reg + "$");
+				if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) {
+					this.value = newValue;
+				}
+				if (event.type == "blur" && this.value.length < 5) {
+					this.value = "";
+				}
+			}
+			elem.addEventListener("input", mask);
+			elem.addEventListener("focus", mask);
+			elem.addEventListener("blur", mask);
+		}
+
+		maskPhone('#form1-phone');
+		maskPhone('#form2-phone');
+		maskPhone('#form3-phone');
+
+		const postData = (body, outputData, errorData, formElements) => {
+			const xhr = new XMLHttpRequest();
+			xhr.addEventListener('readystatechange', () => {
+				if (xhr.readyState !== 4) {
+					return;
+				}
+				if (xhr.status === 200) {
+					outputData();
+				} else {
+					errorData(xhr.status, xhr.statusText);
+				}
+			});
+			xhr.open('POST', '/server.php');
+			xhr.setRequestHeader('Content-Type', 'application/json');
+			xhr.send(JSON.stringify(body));
+			formElements.forEach(item => item.value = '');
+		};
+
+		forms.forEach(form => {
+			const formElements = [...form],
+				reg = /^[^a-zа-я\s]+$/i;
+			form.addEventListener('input', e => {
+				const target = e.target;
+				if (target.tagName.toLowerCase() === 'input' &&
+					(target.id !== 'form1-phone' && target.id !== 'form2-phone' && target.id !== 'form3-phone')) {
+					if (target.value !== '') {
+						target.value = target.value.replace(reg, '');
+					}
+				}
+			});
+			form.addEventListener('submit', e => {
+				e.preventDefault();
+				message.status.classList.add('sk-rotating-plane');
+				form.append(message.status);
+				const formData = new FormData(form);
+				const body = {};
+				formData.forEach((item, index) => {
+					body[index] = item;
+				});
+				postData(body, () => {
+					message.status.classList.remove('sk-rotating-plane');
+					message.status.textContent = message.success;
+				}, (error, errorText) => {
+					message.status.classList.remove('sk-rotating-plane');
+					message.status.textContent = message.error;
+					console.log(error);
+					console.log(errorText);
+				}, formElements);
+			});
+		});
+	};
+
+	sendForm();
 
 
 	// Табы
