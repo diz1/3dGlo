2973a8b8758bf5478123cb6be2efacedfa549e0e
diz1 <53811229+diz1@users.noreply.github.com>
2020-05-09 19:33:00 +0300

    Update countTimer.js

--- src/modules/countTimer.js
+++ src/modules/countTimer.js
@@ -21,6 +21,14 @@ const countTimer = (deadline, hoursElement, minutesElement, secondsElement) => {
 
 	const elements = getElements();
 
+	const addNull = elements => {
+		for (let key in elements) {
+			if (elements[key].textContent.toString().length < 2) {
+				elements[key].textContent = `0${elements[key].textContent.toString()}`;
+			}
+		}
+	};
+
 	const updateClock = () => {
 		const timer = getTimeRemaining();
 
@@ -28,19 +36,9 @@ const countTimer = (deadline, hoursElement, minutesElement, secondsElement) => {
 			elements.timerHours.textContent = `${timer.hours}`;
 			elements.timerMinutes.textContent = `${timer.minutes}`;
 			elements.timerSeconds.textContent = `${timer.seconds}`;
+			addNull(elements);
 
-			if (timer.hours.toString().length === 1) {
-				elements.timerHours.textContent = `0${timer.hours}`;
-			}
-
-			if (timer.minutes.toString().length === 1) {
-				elements.timerMinutes.textContent = `0${timer.minutes}`;
-			}
-
-			if (timer.seconds.toString().length === 1) {
-				elements.timerSeconds.textContent = `0${timer.seconds}`;
-			}
-		} else if (timer.timeRemaining === 0 || timer.timeRemaining < 0) {
+		} else if (timer.timeRemaining <= 0) {
 			elements.timerHours.textContent = '00';
 			elements.timerMinutes.textContent = '00';
 			elements.timerSeconds.textContent = '00';
